2bc2ee9fa4c7947c3d343c0b82d7141e0f3afd73
Showeelik <zeezxex2006@mail.ru>
2024-12-27 01:03:14 +0300

    0.1.5 Обновление настроек проекта Django: изменение имени проекта, добавление CORS и документации API с использованием drf_yasg.

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_django.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "habits_project.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
