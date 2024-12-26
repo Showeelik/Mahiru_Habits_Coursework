b292b07e956b90247fad493f01daee5582142c54
Showeelik <zeezxex2006@mail.ru>
2024-12-27 01:05:26 +0300

    0.1.6 Добавить модель Habit с полями для управления привычками пользователя

---
+++ habits/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Habit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="habits", verbose_name="Создатель")
+    action = models.CharField(max_length=255, verbose_name="Действие")
+    place = models.CharField(max_length=255, verbose_name="Место")
+    time = models.TimeField(verbose_name="Время выполнения")
+    is_pleasant = models.BooleanField(default=False, verbose_name="Приятная привычка")
+    linked_habit = models.ForeignKey(
+        'self', null=True, blank=True, on_delete=models.SET_NULL,
+        limit_choices_to={'is_pleasant': True},
+        verbose_name="Связанная привычка"
+    )
+    reward = models.CharField(max_length=255, blank=True, verbose_name="Вознаграждение")
+    period = models.PositiveSmallIntegerField(default=1, verbose_name="Периодичность (в днях)")
+    duration = models.PositiveIntegerField(verbose_name="Время на выполнение (в секундах)")
+    is_public = models.BooleanField(default=False, verbose_name="Публичная привычка")
+
+    class Meta:
+        verbose_name = "Привычка"
+        verbose_name_plural = "Привычки"
+
+    def __str__(self):
+        return self.action
