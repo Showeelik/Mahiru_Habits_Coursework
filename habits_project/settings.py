5d4cea396b09164d463cdcaba14fc724a400acb4
Showeelik <zeezxex2006@mail.ru>
2024-12-27 01:19:15 +0300

    0.3.1 Добавить класс пагинации HabitPagination и настроить его в настройках REST Framework

--- habits_project/settings.py
+++ habits_project/settings.py
@@ -141,6 +141,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ],
+    "DEFAULT_PAGINATION_CLASS": "habits.pagination.HabitPagination",
 }
 
 # Password validation
